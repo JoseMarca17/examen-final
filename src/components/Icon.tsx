45448b9ee9109ccab64f33e2add0ec2bba68f13e
JoseMarca17 <josemarca1374@gmail.com>
2026-06-08 09:07:59 -0400

    Icono

---
+++ src/components/Icon.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export const Icon: React.FC = () => {
+    return (
+        <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="#666666"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        >
+            <polyline points="9 18 15 12 9 6" />
+        </svg>
+    );
+};
